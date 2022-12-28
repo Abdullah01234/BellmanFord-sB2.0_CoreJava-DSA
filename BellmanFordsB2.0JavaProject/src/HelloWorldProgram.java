7a662ab113d3f509cacf7a3a1da1cfd7ecb9c073
MD ABDULLAH <MD ABDULLAH@192.168.100.8>
2022-12-28 22:56:42 +0530

    first commit

--- BellmanFordsB2.0JavaProject/src/HelloWorldProgram.java
+++ BellmanFordsB2.0JavaProject/src/HelloWorldProgram.java
@@ -1,10 +1,12 @@
-
 public class HelloWorldProgram {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World");
+		System.out.println("Hello World");  //(; = statement terminator)
 
+	//code	
+		
 	}
 
 }
+//Boilerplate code
